2bb99722a38c45ffb71da3c389f3e688cb57e0ec
hexaredecimal <mfanakagama@gmail.com>
2025-11-21 14:46:17 +0200

    Removed old files

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ const { parseArgs } = require('node:util');
 
 
 const appConfig = {
-    name: "rmlc",
+    name: "llml",
     description: "Functional language compiling to JS. Proud fork of Roy",
     commands: {
         build: {
